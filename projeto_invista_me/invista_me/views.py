b3c5f2a8d9022e068d23ef84d2aff7e29743a8de
Aqueiroz2 <40298581+Aqueiroz2@users.noreply.github.com>
2025-01-27 22:26:16 -0400

    atualizacao

---
+++ projeto_invista_me/invista_me/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def pagina_inicial(request):
+    return HttpResponse('Pronto para investir')
+
+def contato(request):
+    return HttpResponse('Para dúvidas, enviar um e-mail para contato@suporte.com')
+
+
